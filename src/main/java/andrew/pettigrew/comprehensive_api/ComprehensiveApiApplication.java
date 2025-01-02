0fc504c1095411ae6dd099fb401d29cb62dd3329
Andrew Pettigrew <devdrew10@gmail.com>
2025-01-02 16:13:53 -0500

    initial commit

---
+++ src/main/java/andrew/pettigrew/comprehensive_api/ComprehensiveApiApplication.java
@@ -0,0 +1,13 @@
+package andrew.pettigrew.comprehensive_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComprehensiveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComprehensiveApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/andrew/pettigrew/comprehensive_api/ComprehensiveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package andrew.pettigrew.comprehensive_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComprehensiveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
