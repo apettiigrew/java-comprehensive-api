439fa5618c26fb66b34003f6d4f77cd2100280c4
Andrew Pettigrew <devdrew10@gmail.com>
2025-01-28 18:20:17 -0500

    fix: add role to user table

--- src/main/java/andrew/pettigrew/comprehensive_api/security/ProjectSecurityConfig.java
+++ src/main/java/andrew/pettigrew/comprehensive_api/security/ProjectSecurityConfig.java
@@ -4,6 +4,7 @@ package andrew.pettigrew.comprehensive_api.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -11,30 +12,21 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 @EnableWebSecurity
 public class ProjectSecurityConfig {
 
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf((csrfConfig)-> csrfConfig.disable())
-        .authorizeHttpRequests((requests) ->
-                requests.requestMatchers("/api/users/register", "/error").permitAll()
-                        .anyRequest().authenticated()
-        );
-        http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests((authorize) ->
+                        authorize.anyRequest().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
+
         return http.build();
     }
-
-//    @Bean
-//    public UserDetailsService userDetailsService(){
-//        UserDetails user = User.withUsername("user").password("1245").authorities("read").build();
-//        UserDetails admin = User.withUsername("admin").password("12456").authorities("admin").build();
-//        return new InMemoryUserDetailsManager(user, admin);
-//    }
 
     @Bean
     PasswordEncoder passwordEncoder(){
--- src/main/resources/db/migration/V1__create_users_table.sql
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -2,6 +2,7 @@ CREATE TABLE users (
     uuid CHAR(36) NOT NULL primary key,
     username VARCHAR(50) not null,
     password VARCHAR(255) not null,
+    role VARCHAR(50) NOT NULL,
     enabled boolean not null,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
