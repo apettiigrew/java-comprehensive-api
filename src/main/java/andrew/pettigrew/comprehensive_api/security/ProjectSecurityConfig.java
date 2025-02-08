ff1b68b94ea2d6f04b2879704d6772f21a42c8ab
Andrew Pettigrew <devdrew10@gmail.com>
2025-02-08 14:08:24 -0500

    feat: fix public register route

--- src/main/java/andrew/pettigrew/comprehensive_api/security/ProjectSecurityConfig.java
+++ src/main/java/andrew/pettigrew/comprehensive_api/security/ProjectSecurityConfig.java
@@ -4,6 +4,7 @@ package andrew.pettigrew.comprehensive_api.security;
 import andrew.pettigrew.comprehensive_api.exceptions.CustomAccessDeniedHandler;
 import andrew.pettigrew.comprehensive_api.exceptions.CustomBasicAuthenticationEntryPoint;
 import andrew.pettigrew.comprehensive_api.security.filter.CsrfCookieFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -18,6 +19,10 @@ import org.springframework.security.web.authentication.password.HaveIBeenPwnedRe
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -29,21 +34,33 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new CsrfTokenRequestAttributeHandler();
-
         http.securityContext(contextConfig -> contextConfig.requireExplicitSave(false))
-                .sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.ALWAYS));
-
-        http.requiresChannel(rcc -> rcc.anyRequest().requiresInsecure()); // Only HTTP
-        http.addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class);
-        http.csrf(csrfConfig -> csrfConfig.csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
-
-                .ignoringRequestMatchers( "/api/register")
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .cors(corsConfig -> corsConfig.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }))
+//        With the given configuration of Spring Security, we can let the framework to generate a
+//        random CSRF token which can be sent to Ul after successful login. The same taken need to be sent by
+//        Ul for every subsequent requests it is making to backend. For certain paths, we can disable CSRF
+//        with the help of ignoringRequestMatchers.
+                .csrf(csrfConfig -> csrfConfig.csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
+                        .ignoringRequestMatchers("/api/register")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+                .requiresChannel(rcc -> rcc.anyRequest().requiresInsecure()) // Only HTTP
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/api/users").authenticated()
-                        .requestMatchers( "/error", "/api/register").permitAll());
+                        .requestMatchers("/api/user/*").authenticated()
+                        .requestMatchers("/api/register").permitAll());
         http.formLogin(withDefaults());
-
         http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
         http.exceptionHandling(ehc -> ehc.accessDeniedHandler(new CustomAccessDeniedHandler()));
         return http.build();
