f9c66595a724f93aad4c603ef217ff5b00380dcb
Andrew Pettigrew <devdrew10@gmail.com>
2025-02-18 11:02:55 -0500

    feat: createa invalid input error messages

--- src/main/java/andrew/pettigrew/comprehensive_api/exceptions/InvoiceErrorResponse.java
+++ src/main/java/andrew/pettigrew/comprehensive_api/exceptions/InvoiceErrorResponse.java
@@ -1,10 +1,15 @@
 package andrew.pettigrew.comprehensive_api.exceptions;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
+import java.util.Map;
+
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class InvoiceErrorResponse {
     private int status;
     private String message;
     private long timeStamp;
+    private Map<String,String> errors;
 }
--- src/main/java/andrew/pettigrew/comprehensive_api/exceptions/InvoiceRestExceptionHandler.java
+++ src/main/java/andrew/pettigrew/comprehensive_api/exceptions/InvoiceRestExceptionHandler.java
@@ -2,11 +2,49 @@ package andrew.pettigrew.comprehensive_api.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @ControllerAdvice
 public class InvoiceRestExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<InvoiceErrorResponse> handleValidationErrors(MethodArgumentNotValidException ex) {
+        List<String> errors = new ArrayList<String>();
+        Map<String,String> errorMap = new HashMap<>();
+
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errorMap.put(getLastField(error.getField()),error.getDefaultMessage());
+        }
+
+        InvoiceErrorResponse error = new InvoiceErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage("Validation failed. Please correct the errors and try again.");
+        error.setErrors(errorMap);
+        error.setTimeStamp(System.currentTimeMillis());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    private Map<String, List<String>> getErrorsMap(List<String> errors) {
+        Map<String, List<String>> errorResponse = new HashMap<>();
+        errorResponse.put("errors", errors);
+        return errorResponse;
+    }
+
+    public static String getLastField(String fieldName) {
+        if (fieldName == null || fieldName.isEmpty()) {
+            return "";
+        }
+        String[] parts = fieldName.split("\\.");
+        return parts[parts.length - 1];
+    }
 
     @ExceptionHandler
     public ResponseEntity<InvoiceErrorResponse> handleException(InvoiceNotFoundException exception){
